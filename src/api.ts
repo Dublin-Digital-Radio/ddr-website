b3c1d04c1395b971fe2b0d60319bbd6f3b7e29cb
Tewson Seeoun <tewson.seeoun@gmail.com>
2024-09-29 19:21:03 +0100

    Add current show time to home page

--- src/api.ts
+++ src/api.ts
@@ -1,3 +1,4 @@
+import { DateTime } from "luxon";
 import { z } from "zod";
 import airtime from "@dublin-digital-radio/airtime-pro-api";
 
@@ -5,17 +6,30 @@ const ddrAirtime = airtime.init({ stationName: "dublindigitalradio" });
 
 const showSchema = z.object({
   name: z.string(),
+  starts: z.string(),
+  ends: z.string(),
 });
 
 export type Show = z.infer<typeof showSchema>;
 
+function formatShow(show: Show) {
+  return {
+    name: show.name,
+    starts: DateTime.fromISO(show.starts.replace(" ", "T")),
+    ends: DateTime.fromISO(show.ends.replace(" ", "T")),
+  };
+}
+
 export async function fetchShows() {
   const response = await ddrAirtime.liveInfoV2();
-  return z
+  const retrievedShows = z
     .object({
       shows: z.object({
         current: showSchema,
       }),
     })
     .parse(response).shows;
+  return {
+    current: formatShow(retrievedShows.current),
+  };
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,16 @@
+import { fetchShows } from "@/api";
 import { Carousel } from "@/components/carousel";
+import { DateTime } from "luxon";
 
-export default function Home() {
+export default async function Home() {
+  const shows = await fetchShows();
   return (
     <main>
+      <div>{shows.current.name}</div>
+      <div>
+        {shows.current.starts.toLocaleString(DateTime.TIME_24_SIMPLE)} -{" "}
+        {shows.current.ends.toLocaleString(DateTime.TIME_24_SIMPLE)}
+      </div>
       <Carousel />
     </main>
   );
