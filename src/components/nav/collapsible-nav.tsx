c3b081307f66fc3734f3212d8b2e0b2290330d54
Tewson Seeoun <tewson.seeoun@gmail.com>
2024-11-15 23:52:20 +0000

    Add nav logo

--- src/components/nav/collapsible-nav.tsx
+++ src/components/nav/collapsible-nav.tsx
@@ -1,9 +1,11 @@
 "use client";
 
 import { Bars3Icon } from "@heroicons/react/24/solid";
+import Image from "next/image";
 import Link from "next/link";
 import { useState } from "react";
 
+import logo from "./logo.png";
 import { NavItem } from "./nav";
 
 export function CollapsibleNav({ navItems }: { navItems: NavItem[] }) {
@@ -14,19 +16,26 @@ export function CollapsibleNav({ navItems }: { navItems: NavItem[] }) {
   };
 
   return (
-    <div>
-      <button onClick={toggleDrawer}>
-        <Bars3Icon className="size-8" />
-      </button>
-      {drawerOpen ? (
-        <ul>
-          {navItems.map(({ href, text }) => (
-            <li key={href}>
-              <Link href={href}>{text}</Link>
-            </li>
-          ))}
-        </ul>
-      ) : null}
+    <div className="flex h-10">
+      <div className="flex-1">
+        <button onClick={toggleDrawer}>
+          <Bars3Icon className="size-8" />
+        </button>
+
+        {drawerOpen ? (
+          <ul>
+            {navItems.map(({ href, text }) => (
+              <li key={href}>
+                <Link href={href}>{text}</Link>
+              </li>
+            ))}
+          </ul>
+        ) : null}
+      </div>
+      <div className="flex flex-1 justify-center p-2">
+        <Image src={logo} alt="ddr logo" height={24} />
+      </div>
+      <div className="flex-1"></div>
     </div>
   );
 }
--- src/components/nav/nav.tsx
+++ src/components/nav/nav.tsx
@@ -1,6 +1,8 @@
+import Image from "next/image";
 import Link from "next/link";
 
 import { CollapsibleNav } from "./collapsible-nav";
+import logo from "./logo.png";
 
 export interface NavItem {
   href: string;
@@ -37,6 +39,11 @@ export async function Nav() {
       </div>
       <div className="hidden md:block">
         <ul className="flex flex-wrap">
+          <li>
+            <div className="w-32 p-4">
+              <Image src={logo} alt="ddr logo" />
+            </div>
+          </li>
           {navItems.map(({ href, text }) => (
             <li key={href}>
               <Link className="me-4" href={href}>
