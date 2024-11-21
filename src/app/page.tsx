5869e44b14bf5b2430488d60805a25ba2a2e39a6
Tewson Seeoun <tewson.seeoun@gmail.com>
2024-11-21 18:38:27 +0000

    Add coming up section to home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,17 @@
 // import { Carousel } from "@/components/carousel";
+import { ComingUp } from "@/components/coming-up";
 import { NowPlaying } from "@/components/now-playing";
 
 export default async function Home() {
   return (
-    <main>
-      <NowPlaying />
+    <main className="flex">
+      <div className="flex-1">
+        <NowPlaying />
+      </div>
       {/* <Carousel /> */}
+      <div className="flex-1">
+        <ComingUp />
+      </div>
     </main>
   );
 }
