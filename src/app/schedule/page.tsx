31f67ac989d9f9320e8a791b343d2e5b35b74b8c
Tewson Seeoun <tewson.seeoun@gmail.com>
2025-01-24 18:51:54 +0000

    Schedule page responsiveness

--- src/app/schedule/page.tsx
+++ src/app/schedule/page.tsx
@@ -7,10 +7,11 @@ export const dynamic = "force-dynamic";
 export default async function Schedule() {
   const schedule = await fetchWeeklySchedule();
   return (
-    <main>
+    <main className="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4">
       {schedule.map((day) => (
-        <div key={day.dayName}>
-          {day.dayName}
+        <div key={day.dayName} className="pb-4">
+          <h1 className="text-xl font-bold uppercase">{day.dayName}</h1>
+
           <ul>
             {day.shows.map((show) => (
               <li key={`${show.name}${show.starts}`}>
