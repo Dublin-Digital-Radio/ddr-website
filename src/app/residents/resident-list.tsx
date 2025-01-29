2aa9117e5c461b4b23807b49f0a5c039ec3873df
Tewson Seeoun <tewson.seeoun@gmail.com>
2025-01-29 18:51:37 +0000

    Loading state for fetching all residents

--- src/app/residents/resident-list.tsx
+++ src/app/residents/resident-list.tsx
@@ -7,15 +7,21 @@ import { fetchAllResidents, fetchResidents, Residents } from "@/api";
 import { SearchForm } from "@/components/search-form";
 
 export function ResidentList({ initResidents }: { initResidents: Residents }) {
+  const [loading, setLoading] = useState(false);
   const [residents, setResidents] = useState(initResidents);
 
   const handleSubmit = useCallback(async (searchQuery: string | undefined) => {
-    const residents = searchQuery
-      ? await fetchResidents({
-          searchQuery,
-        })
-      : await fetchAllResidents();
-    setResidents(residents);
+    if (searchQuery) {
+      await fetchResidents({
+        searchQuery,
+      }).then((residents) => setResidents(residents));
+    } else {
+      setLoading(true);
+      await fetchAllResidents().then((residents) => {
+        setResidents(residents);
+        setLoading(false);
+      });
+    }
   }, []);
 
   return (
@@ -26,27 +32,31 @@ export function ResidentList({ initResidents }: { initResidents: Residents }) {
           onSubmit={handleSubmit}
         />
       </div>
-      <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
-        {residents
-          ?.filter((resident) =>
-            Boolean(resident.attributes.image.data?.attributes.url)
-          )
-          .map((resident) => (
-            <div key={resident.attributes.name}>
-              <Link href={`/resident/${resident.attributes.slug}`}>
-                <div className="aspect-square">
-                  <img
-                    src={resident.attributes.image.data?.attributes.url}
-                    className="w-full h-full object-cover"
-                  />
-                </div>
-                <div className="underline py-2 md:text-2xl md:py-4">
-                  {resident.attributes.name}
-                </div>
-              </Link>
-            </div>
-          ))}
-      </div>
+      {loading ? (
+        <div>Loading...</div>
+      ) : (
+        <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
+          {residents
+            ?.filter((resident) =>
+              Boolean(resident.attributes.image.data?.attributes.url)
+            )
+            .map((resident) => (
+              <div key={resident.attributes.name}>
+                <Link href={`/resident/${resident.attributes.slug}`}>
+                  <div className="aspect-square">
+                    <img
+                      src={resident.attributes.image.data?.attributes.url}
+                      className="w-full h-full object-cover"
+                    />
+                  </div>
+                  <div className="underline py-2 md:text-2xl md:py-4">
+                    {resident.attributes.name}
+                  </div>
+                </Link>
+              </div>
+            ))}
+        </div>
+      )}
     </div>
   );
 }
