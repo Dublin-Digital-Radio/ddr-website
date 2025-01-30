ce9c984508b1f457619346ac0ddba212bf173549
Tewson Seeoun <tewson.seeoun@gmail.com>
2025-01-30 00:08:33 +0000

    Fixed mixcloud player position

--- src/app/mixcloud-player.tsx
+++ src/app/mixcloud-player.tsx
@@ -8,26 +8,28 @@ export function MixcloudPlayer() {
   const { mixcloudIframeUrl, activePlayer } = useContext(NowPlayingContext);
 
   useEffect(() => {
-    if (activePlayer && activePlayer !== 'mixcloud') {
+    if (activePlayer && activePlayer !== "mixcloud") {
       // @ts-expect-error TODO: Add Mixcloud as global var
       const widget = window.Mixcloud.PlayerWidget(
         document.getElementById("mixcloud-iframe")
       );
       widget.ready.then(() => {
-        widget.pause()
+        widget.pause();
       });
     }
   }, [activePlayer]);
 
   return (
-    <iframe
-      id="mixcloud-iframe"
-      frameBorder="0"
-      height="60"
-      width="100%"
-      allow="autoplay"
-      src={mixcloudIframeUrl}
-      className={activePlayer === 'mixcloud' ? 'h-auto' : 'h-0'}
-    />
+    <div className="fixed bottom-0 w-full h-[60px]">
+      <iframe
+        id="mixcloud-iframe"
+        frameBorder="0"
+        height="60"
+        width="100%"
+        allow="autoplay"
+        src={mixcloudIframeUrl}
+        className={activePlayer === "mixcloud" ? "h-auto" : "h-0"}
+      />
+    </div>
   );
 }
