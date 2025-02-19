3655e3ac751fca4ba993035c4291621b3fd70cc7
Tewson Seeoun <tewson.seeoun@gmail.com>
2025-02-19 23:15:44 +0000

    Add live event stream support

--- src/components/now-playing-provider.tsx
+++ src/components/now-playing-provider.tsx
@@ -8,13 +8,26 @@ import {
   useState,
 } from "react";
 
-import { fetchRadioCultLiveShow, fetchRadioCultToggle, Show } from "@/api";
+import {
+  fetchLiveEventStreamConfig,
+  fetchRadioCultLiveShow,
+  fetchRadioCultToggle,
+  Show,
+} from "@/api";
 
 type Player = "stream-1" | "stream-2" | "mixcloud";
+
+interface LiveEventStream {
+  title: string;
+  description?: string;
+  url: string;
+}
 
 const airtimeStreamUrl =
   "https://dublindigitalradio.out.airtime.pro/dublindigitalradio_a";
 const radioCultStreamUrl = "https://dublin-digital-radio.radiocult.fm/stream";
+const defaultLiveEventStreamUrl =
+  "https://stream2.dublindigitalradio.com:8001/stream";
 const defaultMixcloudIframeUrl =
   "https://www.mixcloud.com/widget/iframe/?feed=https%3A%2F%2Fwww.mixcloud.com%2FDublinDigitalRadio%2Fgetting-away-with-it-25th-february-2017%2F&hide_cover=1&mini=1&light=1";
 
@@ -23,6 +36,7 @@ export const NowPlayingContext = createContext<{
   activePlayer?: Player;
   setActivePlayer: Dispatch<SetStateAction<Player | undefined>>;
   currentShow?: Show | null;
+  liveEventStream?: LiveEventStream;
   mixcloudIframeUrl: string;
   setMixcloudIframeUrl: Dispatch<SetStateAction<string>>;
 }>({
@@ -40,6 +54,9 @@ export function NowPlayingProvider({
   const [scheduledStreamUrl, setScheduledStreamUrl] = useState<string>();
   const [activePlayer, setActivePlayer] = useState<Player | undefined>();
   const [currentShow, setCurrentShow] = useState<Show | null | undefined>();
+  const [liveEventStream, setLiveEventStream] = useState<
+    LiveEventStream | undefined
+  >();
   const [mixcloudIframeUrl, setMixcloudIframeUrl] = useState(
     defaultMixcloudIframeUrl,
   );
@@ -47,6 +64,19 @@ export function NowPlayingProvider({
   const fetchAndSetCurrentShowTitle = async () => {
     const liveShow = await fetchRadioCultLiveShow();
     setCurrentShow(liveShow);
+  };
+
+  const fetchAndSetLiveEventStreamConfig = async () => {
+    const liveEventStreamConfig = await fetchLiveEventStreamConfig();
+    setLiveEventStream(
+      liveEventStreamConfig.playerEnabled
+        ? {
+            title: liveEventStreamConfig.Title,
+            description: liveEventStreamConfig.description,
+            url: liveEventStreamConfig.url ?? defaultLiveEventStreamUrl,
+          }
+        : undefined,
+    );
   };
 
   useEffect(() => {
@@ -58,9 +88,15 @@ export function NowPlayingProvider({
       }
     });
     fetchAndSetCurrentShowTitle();
-    setInterval(async () => {
+    fetchAndSetLiveEventStreamConfig();
+    const intervalId = setInterval(async () => {
       fetchAndSetCurrentShowTitle();
+      fetchAndSetLiveEventStreamConfig();
     }, 60 * 1000);
+
+    return () => {
+      clearInterval(intervalId);
+    };
   }, []);
 
   return (
@@ -70,6 +106,7 @@ export function NowPlayingProvider({
         activePlayer,
         setActivePlayer,
         currentShow,
+        liveEventStream,
         mixcloudIframeUrl,
         setMixcloudIframeUrl,
       }}
