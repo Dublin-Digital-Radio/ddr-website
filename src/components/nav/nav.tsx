62e7f3a060453eef123d355de733a96d6f4b2332
Giacomo <giacomo.vergnano@gmail.com>
2025-06-04 00:54:54 +0200

    Add hooks to change nav item style for active page (#24)
    * Add hooks to change nav item style for active page
    
    * Support server rendering of active nav item
    
    ---------
    
    Co-authored-by: Tewson Seeoun <tewson.seeoun@gmail.com>

--- src/components/nav/nav.tsx
+++ src/components/nav/nav.tsx
@@ -1,5 +1,8 @@
+"use client";
+
 import Image from "next/image";
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 
 import { CollapsibleNav } from "./collapsible-nav";
 import logo from "./logo.png";
@@ -8,39 +11,42 @@ export interface NavItem {
   href: string;
   text: string;
   target?: string;
+  activePathnameMatchFunction?: (pathname: string) => boolean;
 }
 
-async function fetchNavItems(): Promise<NavItem[]> {
-  return [
-    {
-      href: "/schedule",
-      text: "Schedule",
+const navItems: NavItem[] = [
+  {
+    href: "/schedule",
+    text: "Schedule",
+  },
+  {
+    href: "/news-events",
+    text: "News + Events",
+  },
+  {
+    href: "/archive",
+    text: "Archive",
+  },
+  {
+    href: "/residents",
+    text: "Residents",
+    activePathnameMatchFunction: (pathname) => {
+      return pathname === "/residents" || pathname.startsWith("/resident/");
     },
-    {
-      href: "/news-events",
-      text: "News + Events",
-    },
-    {
-      href: "/archive",
-      text: "Archive",
-    },
-    {
-      href: "/residents",
-      text: "Residents",
-    },
-    {
-      href: "/about",
-      text: "About",
-    },
-    {
-      href: "/chat-box",
-      text: "Chat Box",
-    },
-  ];
-}
+  },
+  {
+    href: "/about",
+    text: "About",
+  },
+  {
+    href: "/chat-box",
+    text: "Chat Box",
+  },
+];
 
-export async function Nav() {
-  const navItems = await fetchNavItems();
+export function Nav() {
+  const pathname = usePathname();
+
   return (
     <>
       <div className="md:hidden">
@@ -54,13 +60,25 @@ export async function Nav() {
         </div>
         <div className="flex-1 flex justify-end py-4">
           <ul className="flex">
-            {navItems.map(({ href, text, target }) => (
-              <li key={href}>
-                <Link className="me-8 font-bold" href={href} target={target}>
-                  {text}
-                </Link>
-              </li>
-            ))}
+            {navItems.map(
+              ({ href, text, target, activePathnameMatchFunction }) => (
+                <li key={href}>
+                  <Link
+                    className={`me-8 hover:underline ${
+                      activePathnameMatchFunction?.(pathname) ||
+                      pathname === href ||
+                      pathname.startsWith(href)
+                        ? "underline"
+                        : ""
+                    }`}
+                    href={href}
+                    target={target}
+                  >
+                    {text}
+                  </Link>
+                </li>
+              ),
+            )}
           </ul>
         </div>
       </div>
